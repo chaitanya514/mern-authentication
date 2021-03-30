95a95c382fde8d3a69d4dd9a1b1e6398d00923bd
Chaitanya Patel <patelchaitanya514@gmail.com>
2021-03-30 12:01:28 -0400

    mongo db connected

---
+++ server/models/note.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { schema } = require('./user');
+const Schema = mongoose.Schema;
+
+const NoteSchema = new Schema({
+    title:String,
+    body:Object,
+    author:String,
+    videoLink:String,
+    videoTimeStamp:Number,
+    date:{
+        type:Date,
+        default:Date.now
+    }
+},
+    {
+        timestamps:true
+    }
+)
+
+module.exports = Note = mongoose.model('Note',NoteSchema)
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const dbPath = "mongodb+srv://mern-notes:Chaitanya@6514@cluster0.nm9y6.mongodb.net/test";
+
+
+const API_PORT = process.env.PORT || 8080;
+
+
+mongoose.connect(dbPath, {
+    dbName:'chaitanya6514',
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log("mongoDB is connected")
+}).catch(err=>{
+    console.log("Error",err)
+})
+
+app.listen(API_PORT, () => console.log(`listening on ${API_PORT}`))
