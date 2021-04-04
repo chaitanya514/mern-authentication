201dd707e9e812189339f16e685508afb2caabef
Chaitanya Patel <patelchaitanya514@gmail.com>
2021-04-04 16:18:18 -0400

    first router done for homepage

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,24 +8,36 @@ const User = require('../models/user');
 router.post("/", async (req, res) => {
     const { username, password } = req.body;
 
-   if(password.length <6) {
-       res.status(500).json({message:"password must be greater than 6 character"});
-       return;
-   }
+    if (password.length < 6) {
+        res.status(500).json({ message: "password must be greater than 6 character" });
+        return;
+    }
 
-   let newUser = new User({
-    username,
-    passwordHash: bcrypt.hashSync(password,10)
-   })
+    let newUser = new User({
+        username,
+        passwordHash: bcrypt.hashSync(password, 10),
+        numNotes: 0
+    })
 
     newUser
         .save()
         .then(user => {
-            console.log("this is note console:", user)
-            res.json(user);
-            console.log("data with json",res.json(user))
+            jwt.sign({
+                username: newUser.username
+            }, 'secret', (err, token) => {
+                if (err) throw err;
+                res.send({
+                    username: user.username
+                })
+            })
         }).catch(err => {
             console.log("Error caught", err)
-            res.send("Error", err)
+            res.status(500).json({ msg: `User ${err.keyvalue['username']} already exists  ` })
         })
+
+})
+
+router.post("/login",(req,res)=>{
+    const {username,pass}
+
 })
